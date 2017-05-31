180af84d239dd86284fd6f97d616f9f5c06d0371
Juliana Franco <jupvfranco@gmail.com>
2017-05-31 16:52:28 +0100

    script for scalability

---
+++ src/tests/stress/savina/1.pingpong/run-scalability.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+# compile benchmark before
+../../../../../release/encorec --out-file main *.enc 
+
+
+cores=( "0,8"
+	    "0,8,16,24"
+        "0,8,16,24,32,40,48,60"
+        "0,8,16,24,32,40,48,60,2,10,18,26,34,42,50,58"
+        "0,8,16,24,32,40,48,60,2,10,18,26,34,42,50,58,3,11,19,27,35,43,51,59,1,9,17,25,33,41,49,57"
+        "0-63" ) 
+
+RESULT="result.txt" 
+input=$(cat "input.txt")
+
+for c in ${cores[@]}
+do : 
+  numactl -C $c \
+    /usr/bin/time -f "%e" -o "tmp" ./main $input
+    elapsed=$(cat "tmp")
+    echo $c" cores and input "$input" :: "$elapsed >> $RESULT
+done
+
+rm tmp 
+rm main
