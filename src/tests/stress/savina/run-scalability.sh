3f56d55d9b1f65ee911dee7a6d892cb43506734b
UPSCALE partners <upscale@bulldozer.it.uu.se>
2017-06-01 16:19:01 +0200

    first results

--- src/tests/stress/savina/run-scalability.sh
+++ src/tests/stress/savina/run-scalability.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 
 # compile benchmark before
-encorec=./../../../../release/encorec
 main=$(grep "Main" * | tr ':' '\n' | grep ".enc")
-$encorec $main -O3 --out-file main  
+../../../../../release/encorec $main -O3 --out-file main  
 
 cores=( "0,8"
-	    "0,8,16,24"
+	"0,8,16,24"
         "0,8,16,24,32,40,48,60"
         "0,8,16,24,32,40,48,60,2,10,18,26,34,42,50,58"
         "0,8,16,24,32,40,48,60,2,10,18,26,34,42,50,58,3,11,19,27,35,43,51,59,1,9,17,25,33,41,49,57"
         "0-63" ) 
 
+REPETITION=12
 RESULT="result.txt" 
 rm $RESULT
 input=$(cat "input.txt")
 
+ncores=2
 for c in ${cores[@]}
 do : 
-  numactl -C $c \
-    /usr/bin/time -f "%E" -o "tmp" ./main $input
-    elapsed=$(cat "tmp")
-    echo $c" cores and input "$input" :: "$elapsed >> $RESULT
+  it=1
+  while [ $it -le $REPETITION ]
+  do :
+    numactl -C $c \
+      /usr/bin/time -f "%E" -o "tmp" ./main $input
+      elapsed=$(cat "tmp")
+      echo "Cores: "$ncores" :: Input: "$input" :: Iteration: "$it" :: Time: "$elapsed >> $RESULT
+    it=$[$it + 1]
+  done
+  ncores=$[$ncores + $ncores]
 done
 
 rm tmp 
