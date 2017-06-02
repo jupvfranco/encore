8a6e9a3a14c4f12746ddcecd65a050a485cf3b61
Juliana Franco <jupvfranco@gmail.com>
2017-06-02 17:53:30 +0100

    results

--- src/tests/stress/savina/plot-scalability.py
+++ src/tests/stress/savina/plot-scalability.py
@@ -7,22 +7,26 @@ cores = [2, 4, 8, 16, 32, 64]
 
 def plot(ax, encore, akka, name):
   ax.plot(cores, encore, 'bo-', label="Encore")
-#  ax.plot(cores, akka, 'r^-', label="Akka")
+  ax.plot(cores, akka, 'r^-', label="Akka")
   ax.set_title(name)
   ax.set_xlabel('#cores', fontsize=16)
-  ax.set_ylabel('time (millisec)', fontsize=16)
+  ax.set_ylabel('time (sec)', fontsize=16)
 
 def finalisePlot(bm):
   plt.legend(loc='best', shadow=True, fontsize=16)
   plt.savefig(bm + '_scalability.pdf', format='pdf', pad_inches=0, bbox_inches='tight') 
 
-def getMilliSec(t):
+def getSecFromMinutes(t):
   """ 0:04.67"""
   aux=t.split(":")
   minutes=int(aux[0])
   sms=float(aux[1])
   r= float(minutes*60) + sms
   return r
+
+def getSecFromMS(t):
+  """3882.501"""
+  return float(t)/1000
 
 def read_file(fn):
   with open(fn) as f:
@@ -33,6 +37,16 @@ def newRun(di, cores, tt):
   t.append(tt)    
   di[cores] = t 
 
+def final_numbers(results):
+  times=[]
+  for cc in cores:
+    xs = results.get(cc)
+    # Remove two worse times from the 12 executions
+    xs.remove(max(xs))
+    xs.remove(max(xs))
+    times.append(np.average(xs))
+  return times
+
 def parse_encore(fn): 
   """Cores: 2 :: Input:  :: Iteration: 4 :: Time: 0:09.03"""
   results={} # cores --> list of times
@@ -40,26 +54,30 @@ def parse_encore(fn):
     words=line.split(" :: ")
     cc=int(words[0].split(" ")[1])
     iteration=int(words[2].split(" ")[1])
-    tt=getMilliSec(words[3].split(" ")[1])
+    tt=getSecFromMinutes(words[3].split(" ")[1])
     newRun(results, cc, tt)
-  """construct list of times"""
-  times=[]
-  for cc in cores:
-    times.append(np.average(results.get(cc)))
-  return times
+  return final_numbers(results)
 
 def parse_akka(fn):
-  return list()
+  curr_core = 0
+  results={} # cores --> list of times
+  for line in read_file(fn): 
+    if "Core count:" in line: 
+      curr_core = int(line.split(":")[1])
+    elif "Iteration-" in line: 
+      ms = getSecFromMS(line.split(":")[1].replace(" ms", ""))
+      newRun(results, curr_core, ms)
+  return final_numbers(results)
 
 def parseAndPlot(ax, encore_path, akka_path, bm):
-  encore=parse_encore(encore_path + '/result.txt')
-  akka=parse_akka(akka_path + '/result.txt')
+  encore=parse_encore(encore_path)
+  akka=parse_akka(akka_path)
   plot(ax, encore, akka, bm)
 
 f, ax = plt.subplots(1, 1)
-akka_loc='' # TODO
 enc_loc = sys.argv[1]
-benchmark = sys.argv[2]
+akka_loc= sys.argv[2]
+benchmark = sys.argv[3]
 
 parseAndPlot(ax, enc_loc, akka_loc, benchmark)
 finalisePlot(benchmark)
